742f9a30d2eda35dc61330b434777faa07a11894
joshua londono <joshualondono@outlook.com>
2020-08-25 10:12:19 -0400

    added first run storage func

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ if (localStorage.getItem("todosObject") === null) {
 localStorage.setItem('todosObject', JSON.stringify(todoObject));
 }
 }
+
+firstRunStorage()
+
+const storedArray = function(){
+const retrievedObject = localStorage.getItem('todosObject')
+const json = JSON.parse(retrievedObject)
+return Object.values(json)
+}
+
+function save() {
+  const x = document.querySelectorAll("p");
+
+  const todoObject = {
+  mon: x[0].textContent,
+  tues: x[1].textContent,
+  wed: x[2].textContent,
+  thurs: x[3].textContent,
+  friday: x[4].textContent,
+  sat: x[5].textContent,
+  sun: x[6].textContent
+
+};
+
+localStorage.setItem('todosObject', JSON.stringify(todoObject));
+
+}
